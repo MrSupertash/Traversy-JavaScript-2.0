772706ea33e14ce0aa203dd84214ec62a6344396
MrSupertash <mrsupertash@gmail.com>
2023-02-19 16:01:38 +0100

    solution to 1-13 number challenge

---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,14 @@
+const x = Math.ceil(Math.random() * 100);
+const y = Math.ceil(Math.random() * 50);
+
+const sumOutPut = `${x} + ${y} = ${x + y}`;
+const differenceOutPut = `${x} - ${y} = ${x - y}`;
+const productOutPut = `${x} * ${y} = ${x * y}`;
+const quotientOutPut = `${x} / ${y} = ${x / y}`;
+const rmOutPut = `${x} % ${y} = ${x % y}`;
+
+console.log(sumOutPut);
+console.log(differenceOutPut);
+console.log(productOutPut);
+console.log(quotientOutPut);
+console.log(rmOutPut);
