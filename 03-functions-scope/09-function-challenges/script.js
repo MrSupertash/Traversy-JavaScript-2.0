75eeeffef98ffc94f05ba4ed1feea3caf18dcba3
MrSupertash <mrsupertash@gmail.com>
2023-02-19 20:51:05 +0100

    solution to 3-9 challenge 2

--- 03-functions-scope/09-function-challenges/script.js
+++ 03-functions-scope/09-function-challenges/script.js
@@ -15,3 +15,35 @@ const getCelsius = tempFahrenheit => (tempFahrenheit - 32) * 5 / 9;
 console.log(`The temperature is ${getCelsius(120).toFixed(1)} \xB0C`);
 
 
+/*
+## Challenge 2
+
+**Instructions:**
+
+Create an arrow function called `minMax()` that takes in an array of numbers and returns an object with the minimum and maximum numbers in the array.
+
+**Expected Result:**
+
+console.log(minMax([1, 2, 3, 4, 5])); // { min: 1, max: 5 }
+*/
+
+const minMax = arr => {
+    const obj = {
+        min: 0,
+        max: 0,        
+    };
+
+    arr.forEach(num => {
+        if (num < obj.min) {
+            obj.min = num;
+        };
+
+        if (num > obj.max) {
+            obj.max = num;
+        }
+    });
+
+    return obj;
+}
+
+console.log(minMax([1, 23, 3, -4, 5]));
