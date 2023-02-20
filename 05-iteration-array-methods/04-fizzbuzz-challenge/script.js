65a8d651b9ca3e8bb28815367eeea0b125264009
MrSupertash <mrsupertash@gmail.com>
2023-02-20 10:01:16 +0100

    solution to 5-4 FizzBuzz with while loop

--- 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
+++ 05-iteration-array-methods/04-fizzbuzz-challenge/script.js
@@ -9,7 +9,26 @@
 - For numbers which are **multiples of both three and five** print "FizzBuzz".
 */
 
-for (let i = 1; i <= 100; i++) {
+// Solution 1 - For Loop
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+// Solution 2 - While Loop
+
+let i = 1;
+
+while (i <= 100) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuzz');
     } else if (i % 3 === 0) {
@@ -18,5 +37,6 @@ for (let i = 1; i <= 100; i++) {
         console.log('Buzz');
     } else {
         console.log(i);
-    }
-}
+    };
+    i++;
+};
