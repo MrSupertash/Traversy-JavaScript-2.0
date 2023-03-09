e7d39dd333bab3743045cb74939d41315101fbcd
MrSupertash <mrsupertash@gmail.com>
2023-03-09 22:05:31 +0100

    #5 Details Page Backdrop

--- 11-flix-app-project/flixx-app/js/script.js
+++ 11-flix-app-project/flixx-app/js/script.js
@@ -94,8 +94,8 @@ async function displayMovieDetails() {
     const movie = await fetchAPIData(`movie/${movieId}`);
     
 
-    
-
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
 
     const div = document.createElement('div');
     div.innerHTML = `
@@ -133,8 +133,8 @@ async function displayMovieDetails() {
   <div class="details-bottom">
     <h2>Movie Info</h2>
     <ul>
-      <li><span class="text-secondary">Budget:</span> ${movie.budget ? movie.budget.toLocaleString('en-US') : 'N/A'}</li>
-      <li><span class="text-secondary">Revenue:</span> ${movie.revenue ? movie.revenue.toLocaleString('en-US') : 'N/A'}</li>
+      <li><span class="text-secondary">Budget:</span> ${movie.budget ? `$${movie.budget.toLocaleString('en-US')}` : 'N/A'}</li>
+      <li><span class="text-secondary">Revenue:</span> ${movie.revenue ? `$${movie.revenue.toLocaleString('en-US')}` : 'N/A'}</li>
       <li><span class="text-secondary">Runtime:</span> ${movie.runtime ? movie.runtime + ' minutes' : 'N/A'}</li>
       <li><span class="text-secondary">Status:</span> ${movie.status ? movie.status : 'N/A'}</li>
     </ul>
@@ -160,6 +160,28 @@ async function displayMovieDetails() {
     // const companiesText = document.createTextNode(allCompanyNames);
     // document.querySelector('.companies').appendChild(companiesText);
 
+}
+
+// Display Backdrop on Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.2';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else if (type === 'show') {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 
