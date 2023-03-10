b8299f2eb1831f2cf29cb7f5f7af14637e3e4aa4
MrSupertash <mrsupertash@gmail.com>
2023-03-10 11:16:24 +0100

    #6 TV show details

--- 11-flix-app-project/flixx-app/js/script.js
+++ 11-flix-app-project/flixx-app/js/script.js
@@ -162,6 +162,72 @@ async function displayMovieDetails() {
 
 }
 
+
+// Display Show Details
+async function displayShowDetails() {
+  // the following will return the query string, everything after and including the question mark "?" in the URL. We passed that in in the href of the individual cards. We split here at the equal sign, to get the actual value of "?id=4353324" at index 1
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+  
+
+  // Overlay for background image
+  displayBackgroundImage('show', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${  // if show.poster_path is not null/falsy, show image, otherwise show dummy No Image image.
+      show.poster_path
+      ? `<img
+      src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+      : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.name}"
+    />`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i> ${show.vote_average.toFixed(1)}
+    </p>
+    <p class="text-muted">First Air Date: ${show.first_air_date}</p>
+    <p>${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+    ${show.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number Of Seasons:</span> ${show.number_of_seasons ? show.number_of_seasons : 'N/A'}</li>
+    <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes ? show.number_of_episodes : 'N/A'}</li>
+    <li><span class="text-secondary">Last Episode to Air:</span> 
+    ${show.last_episode_to_air.season_number ? `Season ${show.last_episode_to_air.season_number}, ` : ''}
+    ${show.last_episode_to_air.episode_number ? `Episode ${show.last_episode_to_air.episode_number}` : ''}</li>
+    <li><span class="text-secondary">Episode Runtime:</span> ${show.episode_run_time ? show.episode_run_time : 'N/A'}</li>
+    <li><span class="text-secondary">Status:</span> ${show.status ? show.status : 'N/A'}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group companies">
+  ${show.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}
+  </div>
+</div>
+  `
+  document.querySelector('#show-details').appendChild(div);
+}
+
+
 // Display Backdrop on Details Pages
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement('div');
@@ -247,6 +313,7 @@ function init() {
             console.log('Movie Details');
             break;
         case '/tv-details.html':
+            displayShowDetails()
             console.log('TV Details');
             break;
         case '/search.html':
