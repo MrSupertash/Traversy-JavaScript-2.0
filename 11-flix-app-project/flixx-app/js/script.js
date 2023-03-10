2b727207ff3bd8129af67fd06e99990f8667d381
MrSupertash <mrsupertash@gmail.com>
2023-03-10 17:25:34 +0100

    #8 search functionality

--- 11-flix-app-project/flixx-app/js/script.js
+++ 11-flix-app-project/flixx-app/js/script.js
@@ -4,6 +4,18 @@
 // this here sets the global context
 const global = {
     currentPage: window.location.pathname,
+    search: {
+      term: '',
+      type: '',
+      page: 1,
+      totalPages: 1,
+    },
+
+    // DO NOT push the key to GitHUB, delete the key beforehand! Investigate env files and how to gitignore it or something. We should store the key and make requests from a server.
+    api: {
+      apiKey: '',
+      apiUrl: 'https://api.themoviedb.org/3/',
+    },
 };
 
 // Display 20 most popular movies
@@ -251,6 +263,29 @@ function displayBackgroundImage(type, backgroundPath) {
 }
 
 
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  // what we get here is whatever is the value of the respective "name" property on the search.html form. The radio buttons name property has the value of "type", and the input element has the name property of "search-term"
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    const results = await searchAPIData();
+    console.log(results);
+  } else {
+    showAlert('Please enter a search term');
+  }
+
+  console.log(global.search.type);
+  console.log(global.search.term);
+}
+
+
+
+
+
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData('movie/now_playing');
@@ -301,9 +336,8 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-    // DO NOT push the key to GitHUB, delete the key beforehand! Investigate env files and how to gitignore it or something. We should store the key and make requests from a server.
-    const API_KEY = '';    
-    const API_URL = 'https://api.themoviedb.org/3/';
+    const API_KEY = global.api.apiKey;    
+    const API_URL = global.api.apiUrl;
 
     showSpinner();
 
@@ -314,6 +348,22 @@ async function fetchAPIData(endpoint) {
     hideSpinner();
 
     return data;
+}
+
+// Make Request to Search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;    
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
 }
 
 
@@ -342,6 +392,20 @@ function highlightActiveLink() {
 }
 
 
+// Show Alert
+function showAlert(message, className) {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+
+}
+
+
+
+
 // Init App
 function init() {
 
@@ -366,6 +430,7 @@ function init() {
             console.log('TV Details');
             break;
         case '/search.html':
+            search();
             console.log('Search');
             break;
     }
